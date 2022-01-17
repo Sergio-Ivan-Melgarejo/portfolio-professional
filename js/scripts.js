caa77260b54fbf94669a98ee4520cec59251986d
Sergio <sergio.ivan.melgarejo@gmail.com>
2022-01-17 14:54:08 -0300

    added perfil card

--- js/scripts.js
+++ js/scripts.js
@@ -10,12 +10,12 @@ const rootStyle = document.documentElement.style;
 toggleTheme.addEventListener("click",()=>{
     document.body.classList.toggle("dark");
     if(toggleIcon.src.includes("moon.svg")){
-        toggleIcon.src = "assets/icons/sun.svg";
-        toggleText.textContent = "Light Mode";
-    }
-    else{
         toggleIcon.src = "assets/icons/moon.svg";
         toggleText.textContent = "Dark Mode";
+    }
+    else{
+        toggleIcon.src = "assets/icons/sun.svg";
+        toggleText.textContent = "Light Mode";
     }
 })
 
