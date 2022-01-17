6dbe2bbe6ab0928644d958257476c5ce6363baa0
Sergio <sergio.ivan.melgarejo@gmail.com>
2022-01-17 14:09:01 -0300

    add change colors

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,11 @@ const toggleTheme = document.getElementById("toggle-theme");
 const toggleIcon = document.getElementById("toggle-icon");
 const toggleText= document.getElementById("toggle-text");
 
+const toggleColors = document.getElementById("toggle-colors");
+
+const rootStyle = document.documentElement.style;
+
+
 toggleTheme.addEventListener("click",()=>{
     document.body.classList.toggle("dark");
     if(toggleIcon.src.includes("moon.svg")){
@@ -12,4 +17,9 @@ toggleTheme.addEventListener("click",()=>{
         toggleIcon.src = "assets/icons/moon.svg";
         toggleText.textContent = "Dark Mode";
     }
+})
+
+toggleColors.addEventListener("click",(e)=>{
+    console.log(e.target.dataset.color)
+    rootStyle.setProperty("--primary-color", e.target.dataset.color);
 })
