89eb4cef740aaff397de3b570391a76835632cf5
Sergio <sergio.ivan.melgarejo@gmail.com>
2022-01-23 12:17:04 -0300

    added json-data and edit project section

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,8 @@ const rootStyle = document.documentElement.style;
 const flagsElement = document.getElementById("flags");
 const textsToChange = document.querySelectorAll(`[data-section]`);
 
+const containerProject = document.getElementById("card--project");
+
 const changeLanguage = async (language) =>{
     const requestJson = await fetch(`./languages/${language}.json`);
     const texts = await requestJson.json();
@@ -20,6 +22,69 @@ const changeLanguage = async (language) =>{
 
         textToChange.innerHTML = texts[section][value];
     }
+}
+
+const createCards = async () =>{
+    const data = await fetch("../assets/json/data.json");
+    const json = await data.json();
+    json.reverse();
+
+    json.forEach(element => {
+        const card = document.createElement("article");
+        const containerIMG = document.createElement("div");
+        const img = document.createElement("img");
+        const project = document.createElement("div");
+        const projectTags = document.createElement("div");
+        const h2 = document.createElement("h2");
+        const p = document.createElement("p");
+        const buttons = document.createElement("div");
+        const button1 = document.createElement("a");
+        const button2 = document.createElement("a");
+
+        card.classList.add("card");
+        card.classList.add("card--project");
+        containerIMG.classList.add("card__image-container");
+        project.classList.add("project");
+        projectTags.classList.add("project__tags");
+        h2.classList.add("card__title");
+        buttons.classList.add("buttons");
+        button1.classList.add("button");
+        button1.classList.add("button--primary");
+        button2.classList.add("button");
+        button2.classList.add("button--ghost");
+
+        element.data.tags.forEach(ele=>{
+            const span = document.createElement("span");
+            span.classList.add("project__tag");
+            
+            span.textContent = `#${ele}`;
+
+            projectTags.appendChild(span);
+        })
+
+        img.src = element.data.image;
+        containerIMG.appendChild(img);
+        card.appendChild(containerIMG);
+
+        h2.textContent = element.data.title;
+        project.appendChild(h2);
+
+        project.appendChild(projectTags);
+
+        button1.textContent="Demo";
+        button2.textContent="Code";
+        button1.target="__black";
+        button2.target="__black";
+        button1.href=element.data.demo;
+        button2.href=element.data.code;
+        buttons.appendChild(button1);
+        buttons.appendChild(button2);
+        project.appendChild(buttons);
+
+        card.appendChild(project);
+
+        containerProject.appendChild(card);
+    });
 }
 
 flagsElement.addEventListener("click",(e)=>{
@@ -51,4 +116,8 @@ toggleTheme.addEventListener("click",()=>{
 
 toggleColors.addEventListener("click",(e)=>{
     rootStyle.setProperty("--primary-color", e.target.dataset.color);
+})
+
+addEventListener("DOMContentLoaded",()=>{
+    createCards();
 })
